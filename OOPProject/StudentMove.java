1de605edf0a411093e355433df4c348814360072
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2023-12-26 21:29:07 +0600

    StudentMove.java

--- OOPProject/StudentMove.java
+++ OOPProject/StudentMove.java
@@ -1,14 +1,12 @@
-package OOPProject;
+package projects;
 
 import java.util.Vector;
 
 public interface StudentMove {
 	boolean registerCourse(Course course);
-	void viewCourses(Vector<Course> courses);
-	void teacherInfo(Vector<Teacher> teacher);
-	void viewMarks(Vector<Mark> marks);
-	void viewTranscript(Transcript transcript);
-	void rateTeacher(Teacher teacher, double rate);
-	boolean doDiplomaProject();
-	Transcript getTranscript();
+	void viewCourses();
+	void teacherInfo();
+	void viewMarks(String disciplineName);
+	void viewTranscript();
+	void rateTeacher(Teacher teacher, int rating);
 }
