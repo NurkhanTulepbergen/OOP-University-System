e2073b44e0637acfa862d12bd055e497e2e53c82
Yernur Shapkat <113994667+Fruzzy07@users.noreply.github.com>
2023-12-26 20:32:09 +0600

    Mark.java

--- OOPProject/Mark.java
+++ OOPProject/Mark.java
@@ -1,15 +1,36 @@
-package OOPProject;
-
-public class Mark {
+public class Mark implements Serializable {
 	public int grade;
-	public Course course;//Course course nege koldandyk? getMark ta koldanbasak
+	public Course course;
 	public int firstAttest;
 	public int secondAttest;
-	public int total;
+	public int session;
+	public Period semester;
 	
-	public int getMark() {
-		return grade;
+	public Mark() {};
+	
+	public Mark(int grade, Course course) {
+		this.grade = grade;
+		this.course = course;
 	}
+	
+	public Mark(int grade, Course course, int firstAttest, int secondAttest, int session, Period semester) {
+		this.grade = grade;
+		this.course = course;
+		this.firstAttest = firstAttest;
+		this.secondAttest = secondAttest;
+		this.session = session;
+		this.semester = semester;
+		
+	}
+	
+	
+	public Course getCourse() {
+		return course;
+	}
+	
+	public int getGrade() {
+		return firstAttest + secondAttest + session;
+    }
 	
     public static String convertGrade(int grade) {
         if (grade >= 95 && grade <= 100) {
@@ -34,8 +55,39 @@ public class Mark {
         	return "D";
         }
           else {
-            return "Fail";
+            return "F";
         }
     }
+    
+    public static double convertGradeGPA(int grade) {
+        if (grade >= 95 && grade <= 100) {
+            return 4.0;
+        } else if (grade >= 90 && grade < 95) {
+            return 3.67;
+        } else if (grade >= 85 && grade < 90) {
+            return 3.33;
+        } else if (grade >= 80 && grade < 85) {
+            return 3.0;
+        } else if (grade >= 75 && grade < 80) {
+            return 2.67;
+        }else if (grade >= 70 && grade < 75) {
+            return 2.33;
+        } else if (grade >= 65 && grade < 70) {
+            return 2.0;
+        } else if (grade >= 60 && grade < 65) {
+            return 1.67;
+        } else if (grade >= 55 && grade < 60) {
+            return 1.33;
+        } else if(grade >= 50 && grade < 55) {
+        	return 1.0;
+        }
+          else {
+            return 0;
+        }
+    }
+
+	public int getMark() {
+		return grade;
+	}
 	
 }
